44bdc5c5babcae82a0b27b04cb1dddcc1690a001
Maksim Kavalenka <Maksim_Kavalenka@epam.com>
2017-09-29 14:49:39 +0300

    HW2: Remove unnecessary modules; 'add' event on the file watching; Use airbnb-base

--- models/dirwatcher.js
+++ models/dirwatcher.js
@@ -6,11 +6,16 @@ export default class DirWatcher extends EventEmitter {
   watch(path, delay) {
     const watcher = chokidar.watch(path, {
       persistent: true,
-      ignored: '*.txt',
+      ignored: '**/*.txt',
       usePolling: true,
       binaryInterval: delay,
     });
 
+    watcher
+      .on('add', (filePath) => {
+        logger.log('debug', `${filePath} has been added`);
+        this.emit('dirwatcher:changed', filePath);
+      });
     watcher
       .on('change', (filePath) => {
         logger.log('debug', `${filePath} has been changed`);
