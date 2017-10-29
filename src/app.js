535ebdfdc0d78f5b94ceb7ecf03c6210d8b2c9a4
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2017-10-29 20:13:57 +0300

    HW4: Logging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { ExpressMiddleware, echoServer, htmlServer, jsonServer, plainTextServer } from './middlewares';
+import { ProductController, ReviewController, UserController } from './controllers';
+import { ExpressMiddleware, echoServer, htmlServer, jsonServer, plainTextServer, logger } from './middlewares';
 import { productRouter, userRouter } from './routes';
 
 const app = express();
@@ -9,10 +10,34 @@ htmlServer(8082);
 jsonServer(8083);
 echoServer(8084);
 
+app.use((req, res, next) => {
+  logger.debug(`${req.method} ${req.url}`);
+  next();
+});
 app.use(express.json());
 app.use(ExpressMiddleware.cookieParser);
 app.use(ExpressMiddleware.queryParser);
 app.use('/api', productRouter);
 app.use('/api', userRouter);
+
+ProductController.addProduct('product1', 'brand1', 55);
+ProductController.addProduct('product2', 'brand2', 85);
+ProductController.addProduct('product3', 'brand3', 144);
+
+ReviewController.addReview('1', 'author11', 'text11');
+ReviewController.addReview('1', 'author12', 'text12');
+ReviewController.addReview('1', 'author13', 'text13');
+
+ReviewController.addReview('2', 'author21', 'text21');
+ReviewController.addReview('2', 'author22', 'text22');
+ReviewController.addReview('2', 'author23', 'text23');
+
+ReviewController.addReview('3', 'author31', 'text31');
+ReviewController.addReview('3', 'author32', 'text32');
+ReviewController.addReview('3', 'author33', 'text33');
+
+UserController.addUser('user1', 32, 'city1');
+UserController.addUser('user2', 24, 'city2');
+UserController.addUser('user3', 48, 'city3');
 
 module.exports = app;
