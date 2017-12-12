c528c4502fda9c9b357cab3ea6e8a71208ec82db
Maksim Kavalenka <maksim.kavalenka@gmail.com>
2017-12-13 02:38:36 +0300

    HW7: Move mongo-init script into a separate file

--- src/models/product.js
+++ src/models/product.js
@@ -20,6 +20,10 @@ productSchema.pre('update', function (next) {
   this.update({}, { $set: { lastModifiedDate: new Date() } });
   next();
 });
+productSchema.pre('remove', function (next) {
+  this.model.Review.remove({ productId: this._id });
+  next();
+});
 
 const Product = mongoose.model('Product', productSchema);
 
